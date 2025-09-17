0da0430826e7852229af97a97fba2827a2ff46cc
Ivan <243979384@qq.com>
2025-09-17 13:55:54 +0800

    binary search

--- task_01/BinarySearch.sol
+++ task_01/BinarySearch.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.28;
 
-contract BinarySearch {}
+contract BinarySearch {
+    function binarySearch(
+        uint8[] memory arr,
+        uint target
+    ) external pure returns (uint) {
+        uint left;
+        uint right = arr.length;
+
+        while (left < right) {
+            uint middle = left + (right - left) / 2;
+
+            if (target == arr[middle]) {
+                return middle;
+            }
+
+            if (target < arr[middle]) {
+                right = middle;
+            } else {
+                left = middle;
+            }
+        }
+
+        return left;
+    }
+}
