b20eaa576f6c437128b0a15286180c09a616d5d4
Ivan <243979384@qq.com>
2025-09-21 23:21:49 +0800

    task 02 hw-01

---
+++ task_02/ERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+contract ERC20Implement {
+
+}
