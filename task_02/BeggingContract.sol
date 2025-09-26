86acbff66db0090cafe6dadcbfe0ebd9b78de971
Ivan <243979384@qq.com>
2025-09-26 20:27:38 +0800

    homework3

--- task_02/BeggingContract.sol
+++ task_02/BeggingContract.sol
@@ -50,3 +50,86 @@ pragma solidity 0.8.28;
   时间限制：
     添加一个时间限制，只有在特定时间段内才能捐赠
  */
+
+contract BeggingContract {
+    mapping(address => uint256) private donations;
+    address private owner;
+    address[] private donors;
+
+    uint256 public startTime;
+    uint256 public endTime;
+
+    event Donation(address indexed donor, uint256 amount);
+
+    modifier onlyOwner() {
+        _;
+    }
+
+    constructor(uint256 _startTime, uint256 _endTime) {
+        require(_endTime > _startTime, "time expired");
+        owner = msg.sender;
+        startTime = _startTime;
+        endTime = _endTime;
+    }
+
+    // 捐赠
+    function donate() external payable {
+        require(
+            block.timestamp >= startTime && block.timestamp <= endTime,
+            "time expired or not start"
+        );
+        require(msg.value > 0, "donate value must greater than 0");
+
+        if (donations[msg.sender] == 0) {
+            donors.push(msg.sender);
+        }
+        donations[msg.sender] += msg.value;
+
+        emit Donation(msg.sender, msg.value);
+    }
+
+    // 查询某个地址的捐赠金额
+    function getDonation(address donor) external view returns (uint256) {
+        return donations[donor];
+    }
+
+    // 合约所有者提取所有资金
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "no balance");
+        payable(owner).transfer(balance);
+    }
+
+    // 获取合约当前余额
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // 获取排行榜
+    function getTop3()
+        external
+        view
+        returns (address[3] memory topDonors, uint256[3] memory topAmounts)
+    {
+        uint256 donorCount = donors.length;
+
+        for (uint256 i = 0; i < donorCount; i++) {
+            address donor = donors[i];
+            uint256 amount = donations[donor];
+
+            // 检查是否进入前3名
+            for (uint256 j = 0; j < 3; j++) {
+                if (amount > topAmounts[j]) {
+                    // 向后移动数据
+                    for (uint256 k = 2; k > j; k--) {
+                        topAmounts[k] = topAmounts[k - 1];
+                        topDonors[k] = topDonors[k - 1];
+                    }
+                    topAmounts[j] = amount;
+                    topDonors[j] = donor;
+                    break;
+                }
+            }
+        }
+    }
+}
