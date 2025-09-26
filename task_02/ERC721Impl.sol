228707adb47eab6a3222209635d4cffe8f6c2993
Ivan <243979384@qq.com>
2025-09-26 20:14:24 +0800

    homework2

--- task_02/ERC721Impl.sol
+++ task_02/ERC721Impl.sol
@@ -1,42 +1,65 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+/* 
+任务目标
+  使用 Solidity 编写一个符合 ERC721 标准的 NFT 合约。
+  将图文数据上传到 IPFS，生成元数据链接。
+  将合约部署到以太坊测试网（如 Goerli 或 Sepolia）。
+  铸造 NFT 并在测试网环境中查看。
 
-contract ERC721Impl is IERC721 {
-    function supportsInterface(
-        bytes4 interfaceId
-    ) external view returns (bool) {}
+任务步骤
+  编写 NFT 合约
+    使用 OpenZeppelin 的 ERC721 库编写一个 NFT 合约。
+    合约应包含以下功能：
+    构造函数：设置 NFT 的名称和符号。 
+    mintNFT 函数：允许用户铸造 NFT，并关联元数据链接（tokenURI）。
+    在 Remix IDE 中编译合约。
 
-    function balanceOf(address owner) external view returns (uint256 balance) {}
+  准备图文数据
+    准备一张图片，并将其上传到 IPFS（可以使用 Pinata 或其他工具）。
+    创建一个 JSON 文件，描述 NFT 的属性（如名称、描述、图片链接等）。
+    将 JSON 文件上传到 IPFS，获取元数据链接。
+    JSON文件参考 https://docs.opensea.io/docs/metadata-standards
 
-    function ownerOf(uint256 tokenId) external view returns (address owner) {}
+  部署合约到测试网
+    在 Remix IDE 中连接 MetaMask，并确保 MetaMask 连接到 Goerli 或 Sepolia 测试网。
+    部署 NFT 合约到测试网，并记录合约地址。
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes calldata data
-    ) external {}
+  铸造 NFT
+    使用 mintNFT 函数铸造 NFT：
+    在 recipient 字段中输入你的钱包地址。
+    在 tokenURI 字段中输入元数据的 IPFS 链接。
+    在 MetaMask 中确认交易。
+    
+  查看 NFT
+    打开 OpenSea 测试网 或 Etherscan 测试网。
+    连接你的钱包，查看你铸造的 NFT。
+ */
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external {}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-    function transferFrom(address from, address to, uint256 tokenId) external {}
+contract ERC721Impl is ERC721URIStorage {
+    uint private _currentTokenId;
+    address private _owner;
 
-    function approve(address to, uint256 tokenId) external {}
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "no authorized");
+        _;
+    }
 
-    function setApprovalForAll(address operator, bool approved) external {}
+    constructor() ERC721("MyNFT", "MNFT") {
+        _owner = msg.sender;
+    }
 
-    function getApproved(
-        uint256 tokenId
-    ) external view returns (address operator) {}
-
-    function isApprovedForAll(
-        address owner,
-        address operator
-    ) external view returns (bool) {}
+    function mintNFT(
+        address recipient,
+        string memory tokenUri
+    ) public onlyOwner returns (uint) {
+        _currentTokenId += 1;
+        uint nftId = _currentTokenId;
+        _mint(recipient, _currentTokenId);
+        _setTokenURI(_currentTokenId, tokenUri);
+        return nftId;
+    }
 }
